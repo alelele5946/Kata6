6d42b2e7c7e2aaee667f32b8476639b264aad4ab
Alejandro <Alejandro@DESKTOP-ILLTNTP>
2021-12-28 13:37:20 +0000

    Kata6 - versión 6 corregido

--- src/control/Main.java
+++ src/control/Main.java
@@ -9,124 +9,45 @@ import factories.SerialNumberGenerator;
 import business.ToyBusiness;
 import factories.regionalfactories.AmericanToyFactory;
 import factories.regionalfactories.AsianToyFactory;
+import toyproducts.Toy;
 
 public class Main {
 
     public static void main(String[] args) {
         
-        ArrayList<Toy> toys = new ArrayList<>();
-        
-        Scanner in1 = new Scanner (System.in);
-        
+       ToyBusiness business = new ToyBusiness(new AmericanToyFactory());
+       ArrayList<Toy> toys;
+        toys = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         String line = "";
-        
         while(!line.equals("exit")){
             line = in.nextLine();
-            switch (line){
-                
-                case "america":
-                    line = in.nextLine();
-                    AmericanToyFactory americanFactory = new AmericanToyFactory();
-                    ToyBusiness business = new ToyBusiness(americanFactory);
-                    switch(line){
-                        case "car":
-                    
-                        toys.add((AmericanCarToy)business.createToy(line));
-                        System.out.println("Built cars:"+toys.stream()
-                                .map(c -> c.toString())).
-                                collect(Collectors.joining(", ")));
-                            break;
-                    
-                        case "helicopter":
-                    
-                            toys.add((AmericanHelicopterToy)business.produceToy(line));
-                            System.out.println("Built helicopter:"+toys.stream()
-                                    .map(c -> c.toString())).
-                                    collect(Collectors.joining(", ")));
-                    
-                            break;
-                    
-                        case "exit":
-                            System.out.println("Exit...");
-                            break;
-                    
-                        default:
-                            System.out.println("Command unknown");
-                            
-                    }
-                case "asia":
-                    line = in.nextLine();
-                    AsianToyFactory asianFactory = new AsianToyFactory();
-                    ToyBusiness business2 = new ToyBusiness(asianFactory);
-                    switch(line){
-                        case "car":
-                    
-                        toys.add((AmericanCarToy)business2.produceToy(line));
-                        System.out.println("Built cars:"+toys.stream()
-                                .map(c -> c.toString())).
-                                collect(Collectors.joining(", ")));
-                            break;
-                    
-                        case "helicopter":
-                    
-                            toys.add((AmericanHelicopterToy)business2.produceToy(line));
-                            System.out.println("Built helicopter:"+toys.stream()
-                                    .map(c -> c.toString())).
-                                    collect(Collectors.joining(", ")));
-                    
-                            break;
-                    
-                        case "exit":
-                            System.out.println("Exit...");
-                            break;
-                    
-                        default:
-                            System.out.println("Command unknown");
-                            
-                    }
-                    case "exit":
-                            System.out.println("Exit...");
-                            break;
-                    
-                        default:
-                            System.out.println("Command unknown");
-                            
-
-                    
             
-
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
+            switch (line){
+                case "car":
+                case "helicopter":
+                    toys.add(business.produceToy(line));
+                    System.out.println(
+                            "Built toys: "+ toys.stream()
+                                    .map(c -> c.toString())
+                                    .collect(Collectors.joining(", ")));
+                    break;
+                
+                case "exit":
+                    System.out.println("Exiting...");
+                    break;
+                    
+                default:
+                    System.out.println("Command unknown!");
+                    
             
             
             
             }
-            
-        }
         
+        
+        
+        }
         
         
     }
