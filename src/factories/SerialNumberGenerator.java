dcee153e842644af28676e34d79f82679e503dc9
Alejandro <Alejandro@DESKTOP-ILLTNTP>
2021-12-29 12:44:51 +0000

    Kata6 - versión 7

---
+++ src/factories/SerialNumberGenerator.java
@@ -0,0 +1,21 @@
+package factories;
+
+public class SerialNumberGenerator {
+    private Integer serialNumber = 0;
+    /*
+    static private SerialNumberGenerator instance = null ;
+    */
+    /*
+    public static SerialNumberGenerator getInstance(){
+        if(instance == null){
+            instance = new SerialNumberGenerator();
+        }
+        return instance;
+    }
+*/
+    public Integer next() {
+        return this.serialNumber++;
+    
+    }
+    
+}
